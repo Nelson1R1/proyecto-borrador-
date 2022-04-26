f5da57954c8ab788001e10919101b121081f5d89
Nelson1R1 <90815891+Nelson1R1@users.noreply.github.com>
2022-04-26 10:25:07 -0400

    Segundo avance

--- app/models.py
+++ app/models.py
@@ -1,3 +1,45 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+
+class TipoProducto (models.Model):
+    tipo =  models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tipo
+
+class Producto (models.Model):
+    codigo =  models.IntegerField
+    nombre =  models.CharField(max_length=30)
+    marca =  models.CharField(max_length=30)
+    precio =  models.CharField(max_length=30)
+    stock = models.IntegerField
+    tipo =  models.ForeignKey(TipoProducto, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+class TipoUsuario (models.Model):
+    tipoUser = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.tipoUser
+
+class Usuario (models.Model):
+    rut = models.IntegerField
+    nombre = models.CharField
+    apellido = models.CharField
+    correo = models.CharField
+    fechaN = models.DateField
+    tipo = models.ForeignKey(TipoUsuario, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.rut
+
+class Carrito (models.Model):
+    nombre =  models.CharField(max_length=30)
+    marca =  models.CharField(max_length=30)
+    precio =  models.CharField(max_length=30)
+    tipo =  models.ForeignKey(TipoProducto, on_delete= models.CASCADE)
+    codigo = models.ForeignKey(Producto, on_delete= models.CASCADE)
